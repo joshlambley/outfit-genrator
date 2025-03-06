f510ebd3b1579b7c63204b7ac640e02ac098ae35
Josh <105731026+joshlambley@users.noreply.github.com>
2025-03-06 19:25:00 +0000

    new items

--- outfit-generator/src/data/outfits.js
+++ outfit-generator/src/data/outfits.js
@@ -8,16 +8,19 @@ export const outfitData = {
     { img: "/assets/tops/top.png", name: "Arcteryx Jacket"},
     { img: "/assets/tops/top2.png", name: "Grey Sweater"},
     { img: "/assets/tops/top4.webp", name: "Carhartt Jacket"},
-    { img: "/assets/tops/top5.png", name: "CB Tee"}
+    { img: "/assets/tops/top5.png", name: "CB Tee"},
+    { img: "/assets/tops/top6.png", name: "CB Tee"}
   ],
   bottoms: [
     { img: "/assets/bottoms/bottoms1.png", name: "Green Parachute Pants"},
     { img: "/assets/bottoms/bottoms2.png", name: "Grey Sweats"},
     { img: "/assets/bottoms/bottoms3.png", name: "Carpenter Pants"},
-    { img: "/assets/bottoms/bottoms4.png", name: "Carpenter Jeans"}
+    { img: "/assets/bottoms/bottoms4.png", name: "Carpenter Jeans"},
+    { img: "/assets/bottoms/bottoms5.png", name: "Carpenter Jeans"}
   ],
   shoes: [
     { img: "/assets/shoes/shoe1.png", name: "Salomon XT6" },
-    { img: "/assets/shoes/shoe2.png", name: "Addidas Samba" }
+    { img: "/assets/shoes/shoe2.png", name: "Addidas Samba" },
+    { img: "/assets/shoes/shoe3.png", name: "Addidas Samba" }
   ]
 };
